8c66cdcc0c7cfae5bfeb41ce8538cf36f5c4e920
jwu <jwu@efunda.com>
2013-04-13 01:44:35 -0700

    fixed scrolling

--- terminal.js
+++ terminal.js
@@ -12,8 +12,8 @@ function updateTerminal()
 	var cli = document.getElementById('cline');
     $('#prompt').before(cli.value + '</br>');
     cli.value = "";
-    $("#consolation").animate({
-        scrollTop: $("#consolation").height()
+    $("#console").animate({
+        scrollTop: $("#console").height()
     }, 300);
 }
 
