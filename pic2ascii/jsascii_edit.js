b80ee7f3401698ad49703597f0661008d881f79b
Daniel Duan <danielduan88@yahoo.com>
2013-04-12 22:48:56 -0700

    edited

--- pic2ascii/jsascii_edit.js
+++ pic2ascii/jsascii_edit.js
@@ -1,4 +1,6 @@
 /*
+ * Edited by DBOnline for Facebook SoCal Hackathon 2013
+ *
  * jsAscii 0.1
  * Copyright (c) 2008 Jacob Seidelin, jseidelin@nihilogic.dk, http://blog.nihilogic.dk/
  * MIT License [http://www.nihilogic.dk/licenses/mit-license.txt]
