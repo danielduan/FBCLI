c4a987df1662234f22b659156a5e9aebbf9bf51b
Daniel Duan <danielduan88@yahoo.com>
2013-04-13 08:24:38 -0700

    new php img

--- scripts/img.php
+++ scripts/img.php
@@ -1,6 +1,13 @@
 <?php
-$friend_id = $_REQUEST['fid'];
-$picture = "http://graph.facebook.com/" + friend_id + "/picture?type=small";
-header('Content-type: image/jpeg');
-echo file_get_contents($picture);
+$path=$_GET['path'];
+$url="http://graph.facebook.com/"+path+"/picture";
+if (stristr($url, "fbcdn.")==FALSE && stristr($url, "facebook.")==FALSE)
+{
+     echo "ERROR";
+     exit;
+}
+header("Content-Description: Facebook Proxied File");
+header("Content-Type: image");
+header("Content-Disposition: attachment; filename=".$path);
+@readfile($path);
 ?>
