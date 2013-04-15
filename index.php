0fa34efecf3f5eaec5152f7fc59839766b21fea2
Calvin Chan <calvin.chan.h@gmail.com>
2013-04-15 09:20:23 -0700

    Temporarily fixed facebook login issues. The 'like' and 'share' buttons were implemented incorrectly. I removed them for the time being

--- index.php
+++ index.php
@@ -15,13 +15,6 @@
   </head>
   <body>
   <div id="fb-root"></div>
-      <script>(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=443620105724613";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
   <div class="row" id="logo"></div>
   <div id="topbar"><div id="lights"></div><span><div style="display: inline;" class="username">guest</div> &mdash; fbcli &mdash; 100x33</span></div>
   <div class="row" id="console">
